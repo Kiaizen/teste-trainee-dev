dedab54823ed12e79b7d088ddfeb69e0bd534d42
Kiaizen <kiaizenx@gmail.com>
2025-06-09 21:36:48 -0300

    feat: exportar todos

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,24 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../shared/models/todo.model';
 import { TodoService } from '../shared/services/todo.service';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
   todos: Todo[] = [];
   showCompletedTasks: boolean = true;
 
-  constructor(private todoService: TodoService) { }
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
     this.loadTodos();
   }
 
   loadTodos() {
-    this.todoService.getTodos().subscribe(todos => {
+    this.todoService.getTodos().subscribe((todos) => {
       this.todos = todos;
     });
   }
@@ -27,7 +28,7 @@ export class TodoComponent implements OnInit {
     const newTodo: Todo = {
       id: this.todos.length + 1,
       title: newTodoTitle,
-      completed: false
+      completed: false,
     };
 
     this.todoService.addTodo(newTodo);
@@ -42,7 +43,10 @@ export class TodoComponent implements OnInit {
   }
 
   clearAll() {
-    if (this.todos.length > 0 && confirm('Are you sure you want to clear all tasks?')) {
+    if (
+      this.todos.length > 0 &&
+      confirm('Are you sure you want to clear all tasks?')
+    ) {
       this.todoService.clearAll();
       this.loadTodos();
     }
@@ -60,15 +64,40 @@ export class TodoComponent implements OnInit {
   }
 
   filteredTodos() {
-    return this.showCompletedTasks ? this.todos : this.todos.filter(todo => !todo.completed);
+    return this.showCompletedTasks
+      ? this.todos
+      : this.todos.filter((todo) => !todo.completed);
   }
 
-  get labelClearAll(){
-    return 'Limpar Tudo'
+  get labelClearAll() {
+    return 'Limpar Tudo';
   }
-  sortTodos(){
+  sortTodos() {
     this.todoService.sortTodosAZ();
     this.loadTodos();
     this.todos = this.filteredTodos();
+  }
+
+  exportTodos() {
+    const doc = new jsPDF();
+    let y = 10;
+
+    doc.setFontSize(16);
+    doc.text('Lista de Tarefas', 10, y);
+    y += 10;
+
+    this.todos.forEach((todo, index) => {
+      const status = todo.completed ? '[X]' : '[ ]';
+      const text = `${status} ${todo.title}`;
+      doc.text(text, 10, y);
+      y += 8;
+
+      if (y > 280) {
+        doc.addPage();
+        y = 10;
+      }
+    });
+
+    doc.save('lista-de-tarefas.pdf');
   }
 }
